70bb2caf3f83ad7d317a8d0accbae8f313397b6f
vinicius.santos <vinicius.santos@ifood.com.br>
2024-05-24 16:03:18 -0300

    Finish basic project

--- routes/users.js
+++ routes/users.js
@@ -18,11 +18,10 @@ router.post("/cadastro", function (req, res, next) {
   res.redirect("/users");
 });
 
-// router.update("/cadastro", function (req, res, next) {
-//   const { nome, disciplina } = req.body;
-//   const professor = { nome, disciplina };
-//   professores.push(professor);
-//   res.redirect("/users");
-// });
+router.delete("/", function(req, res, next){
+  const professorId = parseInt(req.params.id, 10);
+  professores = professores.filter(p => p.id !== professorId);
+  res.redirect("/users");
+});
 
 module.exports = router;
